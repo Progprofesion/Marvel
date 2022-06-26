0c326bd38c1f1d67b3a2ccea87cba469839fe8ac
Dmitriy <tiemposilencioo@gmail.com>
2022-06-26 19:30:02 +0300

    RandomCharhttpHook

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -3,15 +3,13 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 import Spinner from '../spinner/SpinnerMain'
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
-import MarvelService from '../../services/MarvelService';
+import useMarvelService from '../../services/MarvelService';
 
 const RandomChar = () => {
 
     const [char, setChar] = useState({});
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
 
-    const marvelService = new MarvelService();
+    const { loading, error, getCharacter } = useMarvelService();
 
     useEffect(() => {
         updateChar()
@@ -23,37 +21,15 @@ const RandomChar = () => {
         // eslint-disable-next-line
     }, [])
 
-    // componentDidMount() {
-    //     this.updateChar();
-    //     // this.timerId = setInterval(this.updateChar, 3000)
-    // }
-
-    // componentWillUnmount() {
-    //     clearInterval(this.timerId)
-    // }
-
     const onCharLoaded = (char) => {
         setChar(char);
-        setLoading(loading => false);
     }
 
-    const onCharLoading = () => {
-        setLoading(loading => true)
-    }
-
-
-    const onError = () => {
-        setLoading(loading => false);
-        setError(error => true);
-    }
 
     const updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-        onCharLoading();
-        marvelService
-            .getCharacter(id)
+        getCharacter(id)
             .then(onCharLoaded)
-            .catch(onError)
     }
 
     const errorMessage = error ? <ErrorMessage /> : null;
