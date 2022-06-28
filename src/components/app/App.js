8b5b153f8c0f11e34bfcb83db1b4d40a59ecfeb2
Dmitriy <tiemposilencioo@gmail.com>
2022-06-28 17:09:48 +0300

    routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,43 +1,29 @@
-import { useState } from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import AppBanner from '../appBanner/AppBanner'
-import ComicsList from '../comicsList/ComicsList';
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import { MainPage, ComicsPage, Page404 } from '../pages';
 
-import decoration from '../../resources/img/vision.png';
 
 const App = () => {
 
-    const [selectedChar, setChar] = useState(null)
 
-    const onCharSelected = (id) => {
-        setChar(id)
-
-    }
 
     return (
-        <div className="app">
-            <AppHeader />
-            <main>
-                <AppBanner />
-                <ComicsList />
-                {/* <ErrorBoundary>
-                    <RandomChar />
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected} />
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar} />
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision" /> */}
-            </main>
-        </div>
+        <BrowserRouter>
+            <div className="app">
+                <AppHeader />
+                <main>
+                    <Routes>
+                        <Route path="/" element={<MainPage />} />
+
+                        <Route path="/comics" element={<ComicsPage />} />
+
+                        <Route path='*' element={<Page404 />} />
+                    </Routes>
+
+                </main>
+            </div>
+        </BrowserRouter>
+
     )
 }
 
