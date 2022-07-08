77f207821db930c25355c22b94eba7ecbf2e7767
Dmitriy <tiemposilencioo@gmail.com>
2022-07-08 17:18:43 +0300

    page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,7 +8,9 @@ import Spinner from '../spinner/SpinnerMain';
 const Page404 = lazy(() => import('../pages/404'));
 const MainPage = lazy(() => import('../pages/MainPage'));
 const ComicsPage = lazy(() => import('../pages/ComicsPage'));
-const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+const SingleComicLayout = lazy(() => import('../pages/singleComicLayout/SingleComicLayout'));
+const SingleCharacterLayout = lazy(() => import('../pages/singleCharacterLayout/SingleCharacterLayout'));
+const SinglePage = lazy(() => import('../pages/SinglePage'));
 
 
 
@@ -28,9 +30,11 @@ const App = () => {
 
                             <Route path="/comics" element={<ComicsPage />} />
 
-                            <Route path='*' element={<Page404 />} />
+                            <Route path='/comics/:id' element={<SinglePage Component={SingleComicLayout} dataType='comic' />} />
 
-                            <Route path='/comics/:comicId' element={<SingleComicPage />} />
+                            <Route path='/characters/:id' element={<SinglePage Component={SingleCharacterLayout} dataType='character' />} />
+
+                            <Route path='*' element={<Page404 />} />
                         </Routes>
                     </Suspense>
 
